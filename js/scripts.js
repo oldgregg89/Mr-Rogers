27e7c49010993e5d8ec7a23e1145b8e1f4aaa1d3
Ian Gregg <iangregg188@gmail.com>
2020-06-12 14:25:57 -0700

    Been working on Debugging

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,27 @@ function createArrayOfNumbers(number){
   let newArr=[]
 
   for (var i=0;i<number;i++){
-    newArr.push[i]
+    newArr.push(i)
   }
+  return newArr
 }
 
 // Go through an array and determine if we need to beep boop or wybmn
 function beepBoopNeighbor(arrayOfNumbers){
   let sentence = '';
-
-  for (var i=0;i<number;i++) {
-    if (arrayOfNumbers[i] == 1){
-      sentence.concat("Beep!")
+  for (var i=0;i < arrayOfNumbers.length;i++){
+    if (arrayOfNumbers.indexOf("1")){
+      sentence+="Beep!"
     };
-    if (arrayOfNumbers[i] == 2){
-      sentence.concat("Boop!")
+    if (arrayOfNumbers.includes("2")){
+      sentence+="Boop!"
       
     }
-    if (arrayOfNumbers[i] == 3){
-      sentence.concat("Won't you be my neighbor?")
+    if (arrayOfNumbers.includes("3")){
+      sentence+="Won't you be my neighbor?"
     }
+  }
+  debugger
   return sentence   
 }
 
@@ -32,9 +34,9 @@ function beepBoopNeighbor(arrayOfNumbers){
 $(document).ready(function(){
   $("#numberConverter").submit(function(event) {
     event.preventDefault();
-    let number = $("input#enteredNumber").val();
+    const number = $("input#enteredNumber").val();
     let results = createArrayOfNumbers(number);
-    beepBoopNeighbor(results);
-    $("#output").text(results);
+    let sentence = beepBoopNeighbor(results);
+    $("#output").text(sentence);
   });
-})
+});
