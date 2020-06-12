c4bc6ec1d7092a9d8de0e227ac8316476844008d
Ian Gregg <iangregg188@gmail.com>
2020-06-12 10:02:59 -0700

    Have an updated CSS styles & JavaScript logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 //Business logic
+function numbertoArray(number){
+  let newArr = ""
+  let 
+
+  for (var i=0;i<=number,i++)
+    
+
+}
 
 
 // UI Logic
 $(document).ready(function(){
+  $("#numberConverter").submit(function(event) {
+    event.preventDefault();
+    let number = $("input#enteredNumber").val();
+    let results = numbertoArray(number);
 
+    $("#output").text(results);
+
+  });
 });
